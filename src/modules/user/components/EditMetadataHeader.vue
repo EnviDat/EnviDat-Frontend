42cc8e0507eb63a39b0fcfefa9b08ef4db608e84
Dominik Haas <shaodiese@gmail.com>
2021-06-10 09:44:48 +0200

    added component EditMetadataHeader

---
+++ src/modules/user/components/EditMetadataHeader.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-card id="EditMetadataHeader" >
+
+    <v-card-title >
+      <MetadataHeader />
+    </v-card-title>
+
+    <v-card-text>
+      
+      <input type="text" />
+
+    </v-card-text>
+
+  </v-card>
+</template>
+
+<script>
+/**
+ * MetadataHeader.vue shows the title, authors, keywords,
+ * main contact, doi and license of a metadata entry.
+ *
+ * @summary shows the main infos the a metadata entry
+ * @author Dominik Haas-Artho
+ *
+ * Created at     : 2019-10-23 14:11:27
+ * Last modified  : 2021-01-05 15:55:22
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+*/
+
+// import TagChip from '@/components/Cards/TagChip';
+import MetadataHeader from '@/modules/metadata/components/Metadata/MetadataHeader';
+
+// import { getAuthorName } from '@/factories/authorFactory';
+// import TagChipAuthor from '../TagChipAuthor';
+
+export default {
+  name: 'EditMetadataHeader',
+  props: {
+  },
+  computed: {
+  },
+  methods: {
+  },
+  data: () => ({
+  }),
+  components: {
+    MetadataHeader,
+  },  
+};
+</script>
+
+<style scoped>
+
+
+</style>
--- stories/metadataCreationViews.stories.js
+++ stories/metadataCreationViews.stories.js
@@ -14,8 +14,8 @@
 import { storiesOf } from '@storybook/vue';
 import { action } from '@storybook/addon-actions';
 
-import MetadataHeader from '@/modules/metadata/components/Metadata/MetadataHeader';
-import MetadataBody from '@/modules/metadata/components/Metadata/MetadataBody';
+import EditMetadataHeader from '@/modules/user/components/EditMetadataHeader';
+// import MetadataBody from '@/modules/metadata/components/Metadata/MetadataBody';
 // import MetadataCitation from '@/modules/metadata/components/Metadata/MetadataCitation';
 // import MetadataDetails from '@/modules/metadata/components/Metadata/MetadataDetails';
 // import MetadataLocation from '@/modules/metadata/components/Metadata/MetadataLocation';
@@ -23,131 +23,50 @@ import MetadataBody from '@/modules/metadata/components/Metadata/MetadataBody';
 // import MetadataFunding from '@/modules/metadata/components/Metadata/MetadataFunding';
 // import MetadataAuthors from '@/modules/metadata/components/Metadata/MetadataAuthors';
 
-import doiIcon from '@/assets/icons/doi.png';
-import mailIcon from '@/assets/icons/mail.png';
-import contactIcon from '@/assets/icons/contact2.png';
-import licenseIcon from '@/assets/icons/license.png';
+// import doiIcon from '@/assets/icons/doi.png';
+// import mailIcon from '@/assets/icons/mail.png';
+// import contactIcon from '@/assets/icons/contact2.png';
+// import licenseIcon from '@/assets/icons/license.png';
 
-import {
-  createHeader,
-  createBody,
-} from '@/factories/metaDataFactory';
+// import {
+//   createHeader,
+//   createBody,
+// } from '@/factories/metaDataFactory';
 
 // metadata gets enhance in the storybook config
-import metadata from './js/metadata';
+// import metadata from './js/metadata';
 
-const smallHeader = createHeader(metadata[0], true);
-const largeHeader = createHeader(metadata[1], false);
+// const smallHeader = createHeader(metadata[0], true);
+// const largeHeader = createHeader(metadata[1], false);
 
-const body1 = createBody(metadata[0]);
-const body2 = createBody(metadata[1]);
+// const body1 = createBody(metadata[0]);
+// const body2 = createBody(metadata[1]);
 
-export const methods = {
-  onCardClick: action('clicked on card'),
-  onTagClick: action('clicked on tag'),
-};
+// export const methods = {
+//   onCardClick: action('clicked on card'),
+//   onTagClick: action('clicked on tag'),
+// };
 
 storiesOf('8 Metadata Creation Views / Main Info', module)
   .add('Editing Metadata Header', () => ({
-    components: { MetadataHeader },
+    components: { EditMetadataHeader },
     template: `
     <v-col>
       <v-row>
-        Empty Metadata Header
+        Edit Metadata Header
       </v-row>
 
       <v-row class="py-3" >
         <v-col >
-          <metadata-header metadataId="id-which-can-not-be-found" />
+          <EditMetadataHeader />
         </v-col>
       </v-row>
-
-      <v-row class="py-3" >
-        <v-col >
-          Metadata Header with showPlaceholder
-        </v-col>
-      </v-row>
-
-      <v-row class="py-3" >
-        <v-col >
-          <metadata-header :showPlaceholder="true" />
-        </v-col>
-      </v-row>
-
-      <v-row class="py-3">
-        <v-col >
-          <metadata-header
-            v-bind="smallHeader"
-            :doiIcon="doiIcon"
-            :contactIcon="contactIcon"
-            :mailIcon="mailIcon"
-            :licenseIcon="licenseIcon"
-          />
-        </v-col>
-      </v-row>        
-
-      <v-row class="py-3">
-        <v-col >
-          <metadata-header
-            v-bind="largeHeader"
-            :doiIcon="doiIcon"
-            :contactIcon="contactIcon"
-            :mailIcon="mailIcon"
-            :licenseIcon="licenseIcon"
-          />
-        </v-col>
-      </v-row>        
 
     </v-col>
     `,
-    methods,
+    methods: {
+
+    },
     data: () => ({
-      smallHeader,
-      largeHeader,
-      doiIcon,
-      contactIcon,
-      licenseIcon,
-      mailIcon,
-    }),
-  })).add('Editing Metadata Description', () => ({
-    components: { MetadataBody },
-    template: `
-    <v-row>
-
-      <v-col cols="6" class="py-3">
-        <metadata-body :genericProps="genericPropsPlaceholder" />
-      </v-col>
-
-      <v-col cols="6" class="py-3">
-        <metadata-body :genericProps="genericPropsPlaceholder"
-                        :showPlaceholder="genericPropsPlaceholder.showPlaceholder" />
-      </v-col>
-
-      <v-col cols="4" class="py-3">
-        <metadata-body :genericProps="genericPropsBody" />
-      </v-col>
-
-      <v-col cols="8" class="py-3">
-        <metadata-body :genericProps="genericPropsBodyLongDesc" />
-      </v-col>
-
-    </v-row>        
-    `,
-    methods,
-    data: () => ({
-      genericPropsPlaceholder: {
-        showPlaceholder: true,
-        body: {
-          title: 'Description',
-        },
-      },
-      genericPropsBody: {
-        showPlaceholder: false,
-        body: body1,
-      },
-      genericPropsBodyLongDesc: {
-        showPlaceholder: false,
-        body: body2,
-      },
     }),
   }));
