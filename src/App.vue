1fa1312ebc91af7381dd7a187fd47271b2e05c99
Dominik Haas <shaodiese@gmail.com>
2021-08-03 16:53:57 +0200

    fixed a wrong tag id, just for orientation in the dom

--- src/App.vue
+++ src/App.vue
@@ -65,7 +65,7 @@
         </v-row>
 
         <v-row class="fill-height" 
-                id="maintenanceBanner" >
+                id="mainPageRow" >
           <v-col class="mx-0 py-0"
                   cols="12" >
 
@@ -103,7 +103,7 @@
  * @author Dominik Haas-Artho
  *
  * Created at     : 2019-10-23 16:12:30
- * Last modified  : 2021-01-06 21:04:56
+ * Last modified  : 2021-08-03 12:10:41
  *
  * This file is subject to the terms and conditions defined in
  * file 'LICENSE.txt', which is part of this source code package.
