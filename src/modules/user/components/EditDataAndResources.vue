dfa0ef6eaf49112ef6c0d65bcd7149f0a0cba3c0
Dominik Haas <shaodiese@gmail.com>
2021-08-05 10:42:47 +0200

    added EditDataAndResources main editing component for resources;
    added EditResource component unfinished;

---
+++ src/modules/user/components/EditDataAndResources.vue
@@ -0,0 +1,128 @@
+<template>
+  <v-container fluid
+                class="pa-0"
+                id="EditDataAndResources" >
+    
+    <v-row >
+      <v-col cols="6" >
+        <v-row v-if="selectedResource" >
+          <v-col >
+            <EditResource />
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col>
+            <EditDropResourceFiles @createResources="createResourceFromFiles" />
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col class="text-h5">
+            {{ spacerText }}
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col>
+            <EditPasteResourceUrl @createResources="createResourceFromUrl" />
+          </v-col>
+        </v-row>
+      </v-col>
+
+      <v-col cols="6" >
+        <EditMetadataResources :genericProps="genericProps" />
+      </v-col>
+    </v-row>
+
+  </v-container>
+
+</template>
+
+<script>
+/**
+ * EditDataAndResources.vue shows all the resources of a metadata entry in a list.
+ *
+ * @summary shows the resources the a metadata entry
+ * @author Dominik Haas-Artho
+ *
+ * Created at     : 2019-10-23 14:11:27
+ * Last modified  : 2021-08-04 16:55:23
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+*/
+import {
+  SELECT_EDITING_RESOURCE,
+  SELECT_EDITING_RESOURCE_PROPERTY,
+  eventBus,
+} from '@/factories/eventBus';
+
+
+import { EDIT_METADATA_RESOURCES_TITLE } from '@/factories/metadataConsts';
+import EditMetadataResources from '@/modules/user/components/EditMetadataResources';
+import EditDropResourceFiles from '@/modules/user/components/EditDropResourceFiles';
+import EditPasteResourceUrl from '@/modules/user/components/EditPasteResourceUrl';
+import EditResource from '@/modules/user/components/EditResource';
+
+import { enhanceResourcesStrategyEvents } from '@/factories/strategyFactory';
+
+
+export default {
+  name: 'EditDataAndResources',
+  components: {
+    EditMetadataResources,
+    EditDropResourceFiles,
+    EditPasteResourceUrl,
+    EditResource,
+  },
+  props: {
+    genericProps: Object,
+  },
+  computed: {
+    resources() {
+      const res = this.mixinMethods_getGenericProp('resources', []);
+
+      if (res.length > 0) {
+        enhanceResourcesStrategyEvents(res, SELECT_EDITING_RESOURCE_PROPERTY);
+      }
+
+      return res;
+    },
+    selectedResource() {
+      const id = this.mixinMethods_getGenericProp('selectionId', -1);
+      let selectedRes = null;
+
+      if (id !== -1) {
+        const res = this.resources;
+
+        if (res?.length > 0) {
+          const selected = res.filter(r => r.id === id);
+
+          if (selected?.length > 0) {
+            selectedRes = selected[0];
+          }
+        }
+      }
+
+      return selectedRes;
+    },
+  },
+  methods: {
+    createResourceFromUrl(url) {
+      console.log(`createResourceFromUrl ${url}`);
+    },
+    createResourceFromFiles(files) {
+      console.log(`createResourceFromFiles ${files}`);
+    },
+  },
+  data: () => ({
+    spacerText: 'Or',
+    EDIT_METADATA_RESOURCES_TITLE,
+  }),
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/modules/user/components/EditResource.vue
@@ -0,0 +1,124 @@
+<template>
+  <v-card id="EditResource"
+            class="pa-4">
+
+    <v-container fluid
+                  class="pa-0">
+
+      <v-row>
+        <v-col cols="12"> 
+          <div class="text-h5">{{ labels.title }}</div>
+        </v-col>
+      </v-row>  
+
+      <v-row>
+        <v-col cols="12"> 
+          <div class="text-body-1">{{ labels.instructions }}</div>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="12">
+          <v-textarea :label="labels.description"
+                        outlined
+                        auto-grow
+                        v-model="description"
+                        >
+        </v-textarea>
+
+        </v-col>
+      </v-row>
+      
+      <!-- <v-row>
+        <v-col cols="12"> 
+          <div class="text-body-1">{{ labels.subInstructions }}</div>
+        </v-col>
+      </v-row> -->
+
+      <v-row justify="end">
+        <v-col class="shrink"> 
+          <BaseRectangleButton :disabled="createButtonDisabled"
+                                :buttonText="labels.createButtonText"
+                                @clicked="createButtonClick" />
+        </v-col>
+      </v-row>
+
+
+    </v-container>
+  </v-card>  
+</template>
+
+<script>
+/**
+ * @summary Show all textfields for a resource
+ * @author Dominik Haas-Artho
+ *
+ * Created at     : 2021-06-28 15:55:22
+ * Last modified  : 2021-08-05 10:21:02
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+*/
+import {
+  EDITMETADATA_OBJECT_UPDATE,
+  EDITMETADATA_DATA_RESOURCES,
+  eventBus,
+} from '@/factories/eventBus';
+
+import BaseRectangleButton from '@/components/BaseElements/BaseRectangleButton';
+
+export default {
+  name: 'EditResource',
+  props: {  
+    genericProps: Object,
+  },
+  computed: {
+    description: {
+      get() {
+        return this.mixinMethods_getGenericProp('description', '');
+      },
+      set(value) {
+        const newGenericProps = {
+          ...this.genericProps,
+          description: value,
+        };
+
+        this.notifyChange(newGenericProps);
+      },
+    },
+  },
+  methods: {
+    checkCreateButtonDisabled() {
+      this.createButtonDisabled = this.files?.length <= 0;
+    },
+    notifyChange(newGenericProps) {
+      eventBus.$emit(EDITMETADATA_OBJECT_UPDATE, {
+        object: EDITMETADATA_DATA_RESOURCES,
+        data: newGenericProps,
+      });
+    },
+    createButtonClick() {
+      this.$emit('createResources', this.files);
+    },
+  },
+  data: () => ({
+    labels: {
+      title: 'Edit Selected Resource',
+      instructions: 'Drag and drop a file here to create a new resource and upload it. For files with a file size < 5 GB.',
+      subInstructions: 'For files larger then 5GB contact the envidat team.',
+      createButtonText: 'Save Resource',
+      description: 'Resource Description',
+    },
+    files: [],
+    createButtonDisabled: true,
+  }),
+  components: {
+    BaseRectangleButton,
+  },  
+};
+</script>
+
+<style scoped>
+
+
+</style>
