d2ad60e5a95f0f831b5fd200eb1d0b66d6889b82
Dominik <shaodiese@gmail.com>
2021-08-19 16:58:30 +0200

    added basic OrganizationTree component and testdata

---
+++ src/modules/user/components/OrganizationTree.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-card id="OrganizationTree"
+            class="pa-4" >
+
+      
+    <v-sheet class="pa-4 primary lighten-2">
+      <v-text-field
+        v-model="search"
+        label="Search Company Directory"
+        dark
+        flat
+        solo-inverted
+        hide-details
+        clearable
+        clear-icon="mdi-close-circle-outline"
+      ></v-text-field>
+      <v-checkbox
+        v-model="caseSensitive"
+        dark
+        hide-details
+        label="Case sensitive search"
+      ></v-checkbox>
+    </v-sheet>
+    <v-card-text>
+      <v-treeview :items="items"
+                  :search="search"
+                  :filter="filter"
+                  :open.sync="open"
+                >
+        <template v-slot:prepend="{ item }">
+          <v-icon
+            v-if="item.children"
+            v-text="`mdi-${item.id === 1 ? 'home-variant' : 'folder-network'}`"
+          ></v-icon>
+        </template>
+      </v-treeview>
+    </v-card-text>
+
+  </v-card>  
+</template>
+
+<script>
+/**
+ * @summary Shows a treeview of the organisations
+ * @author Dominik Haas-Artho
+ *
+ * Created at     : 2021-08-19 16:09:39
+ * Last modified  : 2021-08-19 16:09:39
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+*/
+
+export default {
+  name: 'OrganizationTree',
+  props: {  
+    genericProps: Object,
+  },
+  mounted() {
+  },
+  computed: {
+  },
+  methods: {
+  },
+  data: () => ({
+    localName: '',
+  }),
+  components: {
+  },  
+};
+</script>
+
+<style scoped>
+
+
+</style>
