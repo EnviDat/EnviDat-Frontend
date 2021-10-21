adc6c9df1d81f0cf5ce3f29c6785399ca4f6804e
Dominik Haas <shaodiese@gmail.com>
2021-10-21 17:23:21 +0200

    Validations for Publication Information, fields and funderslist

--- src/modules/user/components/EditPublicationInfo.vue
+++ src/modules/user/components/EditPublicationInfo.vue
@@ -20,8 +20,8 @@
         <v-col cols="6">
           <v-text-field :label="labels.publicationState"
                         outlined
-                        required
                         readonly
+                        :error-messages="validationErrors.publicationState"
                         v-model="publicationStateField" />
         </v-col>
 
@@ -33,6 +33,7 @@
         <v-col cols="6">
           <v-text-field :label="labels.dataObjectIdentifier"
                         outlined
+                        :error-messages="validationErrors.doi"
                         prepend-icon="fingerprint"
                         v-model="doiField" />
         </v-col>
@@ -57,10 +58,9 @@
 
         <v-col cols="6">
           <v-text-field :label="labels.publisher"
-                        readonly
                         outlined
                         required
-                        :rules="rulesPublisher"
+                        :error-messages="validationErrors.publisher"
                         prepend-icon="public"
                         v-model="publisherField" />
         </v-col>
@@ -69,6 +69,7 @@
           <v-select :items="yearList"
                     outlined
                     :label="labels.year"
+                    :error-messages="validationErrors.publicationYear"
                     required
                     prepend-icon="date_range"
                     v-model="publicationYearField"
@@ -94,6 +95,7 @@
               <v-text-field :label="labels.organization"
                             outlined
                             :value="item.organization"
+                            :error-messages="validationErrors.funders[index].organization"
                             @input="notifyChange(index, 'organization', $event)" />
 
             </v-col>
@@ -101,22 +103,24 @@
               <v-text-field :label="labels.grantNumber"
                             outlined
                             :value="item.grantNumber"
+                            :error-messages="validationErrors.funders[index].grantNumber"
                             @input="notifyChange(index, 'grantNumber', $event)" />
             </v-col>
             <v-col cols="4">
               <v-text-field :label="labels.link"
                             outlined
                             :value="item.link"
+                            :error-messages="validationErrors.funders[index].link"
                             @input="notifyChange(index, 'link', $event)" />
             </v-col>
 
       </v-row>
 
 
-      <v-row v-if="maxFundersReached">
+      <v-row v-if="validationErrors.fundersArray">
 
         <v-col cols="12">
-          <div class="text-subtitle-2"><span class="red--text">{{ this.maxFundersMessage }}</span></div>
+          <div class="text-subtitle-2"><span class="red--text">{{ validationErrors.fundersArray }}</span></div>
         </v-col>
 
       </v-row>
@@ -150,6 +154,13 @@ import {
   isMaxLength,
 } from '@/factories/userEditingFactory';
 
+import { validationMixin } from 'vuelidate';
+import {
+  required,
+  minLength,
+  url,
+} from 'vuelidate/lib/validators';
+
 import { mapState } from 'vuex';
 
 
@@ -159,28 +170,6 @@ export default {
     this.getCurrentYear();
     this.getYearslist();
   },
-  data: () => ({
-    labels: {
-      cardTitle: 'Publication Info',
-      publicationState: 'Publication State',
-      dataObjectIdentifier: 'Data Object Identifier',
-      publisher: 'Publisher',
-      year: 'Year',
-      fundingInformation: 'Funding Information',
-      organization: 'Organization',
-      grantNumber: 'Grant Number',
-      link: 'Link',
-    },
-    buttonColor: '#269697',
-    rulesPublisher: [v => !!v || 'Publisher is required'],
-    currentYear: '',
-    yearList: [],
-    maxFundersReached: false,
-    defaultUserEditMetadataConfig: {
-      publicationMaxFunders: 5,
-      publicationYearsList: 30,
-    },
-  }),
   props: {
     publicationState: {
       type: String,
@@ -204,7 +193,7 @@ export default {
     },
   },
   computed: {
-      ...mapState([
+    ...mapState([
       'config',
     ]),
     maxFunders() {
@@ -226,7 +215,18 @@ export default {
         return this.doi;
       },
       set(value) {
-        this.setPublicationInfo('doi', value);
+        const property = 'doi';
+
+        if (!this.isFieldValid(property, value, this.propertyValidationSuffix)) {
+          if (this.$v[`${property}${this.propertyValidationSuffix}`].minLength === false) {
+            this.validationErrors[property] = `${property} must have at least ${this.$v[`${property}${this.propertyValidationSuffix}`].$params.minLength.min}`;
+          }
+          return;
+        }
+
+        this.validationErrors[property] = '';
+
+        this.setPublicationInfo(property, value);
       },
     },
     publisherField: {
@@ -234,7 +234,18 @@ export default {
         return this.publisher;
       },
       set(value) {
-        this.setPublicationInfo('publisher', value);
+        const property = 'publisher';
+
+        if (!this.isFieldValid(property, value, this.propertyValidationSuffix)) {
+          if (!this.$v[`${property}${this.propertyValidationSuffix}`].minLength === false) {
+            this.validationErrors[property] = `${property} must have at least ${this.$v[`${property}${this.propertyValidationSuffix}`].$params.minLength.min}`;
+          }
+          return;
+        }
+
+        this.validationErrors[property] = '';
+
+        this.setPublicationInfo(property, value);
       },
     },
     publicationYearField: {
@@ -316,10 +327,17 @@ export default {
             link: '',
           },
         );
+        this.validationErrors.funders.push(
+          {
+            organization: '',
+            grantNumber: '',
+            link: '',
+          },
+        );
       }
     },
     // Assign localFunders to a copy of funderArray with last empty funder object removed
-    copyFunderArray(localFunders) {
+    removeUsedEntry(localFunders) {
 
       const lastFunder = localFunders[localFunders.length - 1];
 
@@ -342,6 +360,59 @@ export default {
         [property]: value,
       };
     },
+    isArrayValid(array, arrayProperty, index, valueProptery, propertyValidationSuffix = 'Validation') {
+      const propertyString = `${arrayProperty}${propertyValidationSuffix}`;
+
+      this.$set(this, propertyString, array);
+
+      const vuelidateObj = this.$v[propertyString];
+
+      vuelidateObj.$touch();
+
+      if (vuelidateObj.$error && !vuelidateObj.minLength) {
+        this.validationErrors.fundersArray = `At least ${this.$v[`${arrayProperty}${this.propertyValidationSuffix}`].$params.minLength.min} of funders has to be defined`;
+        return false;
+      }
+
+      this.validationErrors.fundersArray = null;
+
+      const vuelidateValueObj = this.$v[propertyString].$each[index][valueProptery];
+
+      let errorMsg = '';
+      if (vuelidateValueObj.$error) {
+        if (vuelidateValueObj.required === false) {
+        errorMsg = `${valueProptery} is required`;
+        }
+
+        if (vuelidateValueObj.minLength === false) {
+          errorMsg = `${valueProptery} must have at least ${vuelidateValueObj.$params.minLength.min}`;
+        }
+
+        if (vuelidateValueObj.url === false) {
+          errorMsg = `${valueProptery} must be a url`;
+        }
+      }
+
+      const errorArray = this.validationErrors[arrayProperty];
+      if (errorArray[index] !== null){
+        errorArray[index][valueProptery] = errorMsg;
+      }
+
+      this.$set(this.validationErrors, arrayProperty, errorArray);
+
+      return !vuelidateObj.$invalid;
+    },
+    isFieldValid(property, value, propertyValidationSuffix = 'Validation', checkRequired = true) {
+      const propertyString = `${property}${propertyValidationSuffix}`;
+      this[propertyString] = value;
+      this.$v[propertyString].$touch();
+
+      if (checkRequired && this.$v[propertyString].$error && !this.$v[propertyString].required) {
+        this.validationErrors[property] = `${property} is required`;
+      }
+
+      return !this.$v[propertyString].$invalid;
+    },
     setPublicationInfo(property, value) {
 
       const newPublicationInfo = {
@@ -362,14 +433,89 @@ export default {
 
       deleteEmptyObject(index, localyCopy);
 
-      this.copyFunderArray(localyCopy);
+      this.removeUsedEntry(localyCopy);
 
-      this.setPublicationInfo('funders', localyCopy);
+      if (this.isArrayValid(localyCopy, 'funders', index, property)) {
+        this.setPublicationInfo('funders', localyCopy);
+      }
 
-      this.maxFundersReached = isMaxLength(this.maxFunders, localyCopy);
-
+      if (isMaxLength(this.maxFunders, localyCopy)) {
+        this.validationErrors.fundersArray = this.maxFundersMessage;
+      }
     },
   },
+  validations: {
+    publicationStateValidation: {
+      required,
+    },
+    doiValidation: {
+      required,
+      minLength: minLength(3),
+    },
+    publisherValidation: {
+      required,
+      minLength: minLength(3),
+    },
+    publicationYearValidation: {
+      required,
+    },
+    fundersValidation: {
+      required,
+      minLength: minLength(1),
+      $each: {
+        organization: {
+          required,
+          minLength: minLength(3),
+        },
+        grantNumber: {
+          minLength: minLength(3),
+        },
+        link: {
+          url,
+        },
+      },
+    },
+  },
+  data: () => ({
+    labels: {
+      cardTitle: 'Publication Info',
+      publicationState: 'Publication State',
+      dataObjectIdentifier: 'Data Object Identifier',
+      publisher: 'Publisher',
+      year: 'Year',
+      fundingInformation: 'Funding Information',
+      organization: 'Organization',
+      grantNumber: 'Grant Number',
+      link: 'Link',
+    },
+    publicationStateValidation: '',
+    doiValidation: '',
+    publisherValidation: '',
+    publicationYearValidation: '',
+    fundersValidation: '',
+    propertyValidationSuffix: 'Validation',
+    validationErrors: {
+      publicationState: null,
+      doi: null,
+      publisher: null,
+      publicationYear: null,
+      funders: [{
+        organization: '',
+        grantNumber: '',
+        link: '',
+      }],
+      fundersArray: null,
+    },
+    buttonColor: '#269697',
+    rulesPublisher: [v => !!v || 'Publisher is required'],
+    currentYear: '',
+    yearList: [],
+    defaultUserEditMetadataConfig: {
+      publicationMaxFunders: 5,
+      publicationYearsList: 30,
+    },
+  }),
+  mixins: [validationMixin],
   components: {
   },
 };
