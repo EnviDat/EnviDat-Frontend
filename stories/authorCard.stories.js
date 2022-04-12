ac1bd34831f0ef25ddc90e53111678679541456b
Haas <dominik.haas@wsl.ch>
2022-04-12 20:41:44 +0200

    updated authorCard story

--- stories/authorCard.stories.js
+++ stories/authorCard.stories.js
@@ -1,5 +1,4 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { storiesOf } from '@storybook/vue';
 /*
 import {
  withKnobs, text, number,
@@ -7,13 +6,15 @@ import {
 */
 
 import AuthorCard from '@/modules/metadata/components/AuthorCard';
+import DataCreditLayout from '@/components/Layouts/DataCreditLayout';
 import authorCollection from '../public/testdata/authorCollection.json';
+import { CARD_VIEWS } from './storybookFolder';
 
 const authorFromCollection = Object.values(authorCollection)[1];
 const authorFromCollection2 = Object.values(authorCollection)[5];
 const authorFromCollection3 = Object.values(authorCollection)[4];
 
-export const methods = {
+const methods = {
   authors() {
     const items = Object.values(authorCollection);
     return items.splice(0, items.length / 2);
@@ -21,10 +22,17 @@ export const methods = {
 };
 
 
-// const stories = storiesOf('3 Cards / Author Cards', module)
-storiesOf('3 Cards / Author Cards', module)
-  .add('Author Cards', () => ({
-    components: { AuthorCard },
+export default {
+  title: `${CARD_VIEWS}`,
+  decorators: [],
+  parameters: {},
+};
+
+export const AuthorCardViews = () => ({
+  components: {
+    AuthorCard,
+    DataCreditLayout,
+  },
 /*
     props: {
       author: {
@@ -50,54 +58,94 @@ storiesOf('3 Cards / Author Cards', module)
       },
     },
 */
-    template: `
-    <v-container grid-list-lg fluid pa-0>
-      <v-row>
+  template: `
+  <v-container grid-list-lg fluid pa-0>
+    <v-row>
 
-        <v-col cols="12" md="4" pt-5 >
-          <author-card :author="author" />
-        </v-col>
+      <v-col cols="12" md="4" pt-5 >
+        {{ datasetDataCredit }}
+        <author-card :author="authorFromCollection" >
+          <template #default >
 
-        <v-col cols="12" md="4" pt-5 >
-          <author-card :author="authorFromCollection" />
-        </v-col>
+            <DataCreditLayout class="px-0 py-1 readableText"
+                              :dataCredit="datasetDataCredit"
+                              badgesLabel="DataCredits for this Entry"
+                              iconColor="white"
+                              badgeColor="#384753"
+                              :dark="true" />
 
-        <v-col cols="12" md="4" pt-5 >
-          <author-card :author="authorFromCollection2" :loading="true" />
-        </v-col>
+          </template>
+        </author-card>
+      </v-col>      
+<!--
+      <v-col cols="12" md="4" pt-5 >
+        <author-card :author="author" />
+      </v-col>
 
-        <v-col cols="12" md="4" pt-5 >
-          <author-card :author="authorFromCollection3" />
-        </v-col>
+      <v-col cols="12" md="4" pt-5 >
+        <author-card :author="authorFromCollection" >
+          <template #default="{ getCurrentDataCredit }" >
+            {{ getCurrentDataCredit }}
 
-      </v-row>
-    </v-container>
-    `,
-    methods,
-    data: () => ({
-      authorFromCollection,
-      authorFromCollection2,
-      authorFromCollection3,
-      author: {
-        firstName: 'Felix',
-        lastName: 'Gugerli',
-        fullName: 'Felix Gugerli',
-        datasetCount: 7,
-        affiliation: 'WSL',
-        id: {
-          identifier: '0000-0003-3878-1845',
-        },
-        email: 'felix.gugerli@wsl.ch',
-        dataCredit: {
-          collection: 10,
-          validation: 3,
-          curation: 12,
-          software: 10,
-          publication: 15,
-          supervision: 1,
-        },
+            <DataCreditLayout class="px-0 py-1 readableText"
+                              :dataCredit="getCurrentDataCredit"
+                              badgesLabel="DataCredits for this Entry"
+                              iconColor="white"
+                              badgeColor="black"
+                              :dark="true" />
+
+
+          </template>
+        </author-card>
+      </v-col>
+
+
+      <v-col cols="12" md="4" pt-5 >
+        <author-card :author="authorFromCollection2" :loading="true" />
+      </v-col>
+
+      <v-col cols="12" md="4" pt-5 >
+        <author-card :author="authorFromCollection3" />
+      </v-col>
+-->
+
+    </v-row>
+  </v-container>
+  `,
+  methods,
+  computed: {
+    getCurrentDataCredit() {
+      return this.datasetDataCredit;
+    },
+  },
+  data: () => ({
+    authorFromCollection,
+    authorFromCollection2,
+    authorFromCollection3,
+    datasetDataCredit: {
+      collection: 1,
+      software: 2,
+    },
+    author: {
+      firstName: 'Felix',
+      lastName: 'Gugerli',
+      fullName: 'Felix Gugerli',
+      datasetCount: 7,
+      affiliation: 'WSL',
+      id: {
+        identifier: '0000-0003-3878-1845',
       },
-    }),
-  }));
+      email: 'felix.gugerli@wsl.ch',
+      dataCredit: {
+        collection: 10,
+        validation: 3,
+        curation: 12,
+        software: 10,
+        publication: 15,
+        supervision: 1,
+      },
+    },
+  }),
+});
 
 // stories.addDecorator(withKnobs);
--- stories/storybookFolder.js
+++ stories/storybookFolder.js
@@ -1,3 +1,4 @@
+export const CARD_VIEWS =  '3 Cards';
 export const GCNET_VIEWS =  '8 GC-Net Views';
 export const METADATA_EDITING = '9 Editing Metadata';
 export const CHIPS = '10 CHIPS';
