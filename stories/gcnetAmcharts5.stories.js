8fdc18f60080f872dc20be249a794a14213419db
Haas <dominik.haas@wsl.ch>
2022-09-28 18:09:52 +0200

    feat(EditChart): added story for Editchart, it's WIP and might break

--- stories/gcnetAmcharts5.stories.js
+++ stories/gcnetAmcharts5.stories.js
@@ -17,11 +17,16 @@
 
 import { convertCSVToJSON, getChartSeries } from '@/factories/stringFactory';
 import axios from 'axios';
-import * as am5 from '@amcharts/amcharts5';
-import am5themesAnimated from '@amcharts/amcharts5/themes/Animated';
-import * as am5xy from '@amcharts/amcharts5/xy';
+import Amcharts5 from '@/modules/metadata/components/GC-Net/Amchart5Chart.vue';
+import EditChart from '@/modules/user/components/edit/EditChart.vue';
+import { createChart } from '@/factories/chartFactory';
 import { CHART_VIEWS } from './storybookFolder';
-import Amcharts5 from '../src/modules/metadata/components/GC-Net/Amchart5Chart';
+import * as am5 from '@amcharts/amcharts5';
+
+// import neadContent from '../public/testdata/00-Swiss Camp 10m.csv';
+// import researchUnits from '../public/researchUnits.json';
+
+// console.log(neadContent);
 
 export default {
   title: `${CHART_VIEWS} / AmChart5 Tests`,
@@ -145,6 +150,7 @@ export const Amcharts5LargeData = () => ({
   }),
 });
 
+
 export const NEADAutoChart = () => ({
   components: { Amcharts5 },
   template: `
@@ -230,113 +236,7 @@ export const NEADAutoChart = () => ({
     loadChart(chartNumber = 0) {
       const series = getChartSeries([this.neadJSON?.parameters[chartNumber]], this.neadJSON?.data);
 
-      this.createChart(`neadAutoChart_${chartNumber}`, this.neadJSON?.parameters[chartNumber], series);
-    },
-    createChart(yAxisDivID, yAxisName, data) {
-
-      const root = am5.Root.new(yAxisDivID);
-
-      // Set all dates in root to UTC
-      // NOTE: It is critical to set the root to UTC, otherwise timestamps will be rendered in local time!!!!
-      root.utc = true;
-
-      root.setThemes([am5themesAnimated.new(root)]);
-
-      // Create chart
-      const chart = root.container.children.push(
-        am5xy.XYChart.new(root, {
-          focusable: true,
-          panX: true,
-          panY: true,
-          wheelX: 'panX',
-          wheelY: 'zoomX',
-        }),
-      );
-
-      // const easing = am5.ease.linear;
-
-      // Create axes
-      const xAxis = chart.xAxes.push(
-        am5xy.DateAxis.new(root, {
-          maxDeviation: 0.1,
-          groupData: true,
-          groupCount: 500,
-          baseInterval: {
-            timeUnit: 'hour',
-            count: 1,
-          },
-          renderer: am5xy.AxisRendererX.new(root, {
-            // minGridDistance: 50,
-          }),
-          tooltip: am5.Tooltip.new(root, {}),
-        }),
-      );
-
-      const yAxis = chart.yAxes.push(
-        am5xy.ValueAxis.new(root, {
-          maxDeviation: 0.1,
-          renderer: am5xy.AxisRendererY.new(root, {}),
-          tooltip: am5.Tooltip.new(root, {}),
-        }),
-      );
-
-      // Set cursor
-      chart.set('cursor', am5xy.XYCursor.new(root, {
-        behavior: 'zoomX',
-        xAxis,
-      }));
-      // cursor.lineY.set('visible', false);
-
-      // Add scrollbar
-      chart.set('scrollbarX', am5.Scrollbar.new(root, {
-        orientation: 'horizontal',
-      }));
-
-/*
-      chart.appear(1000, 100);
-*/
-
-
-      // Add series
-      const series = chart.series.push(
-        am5xy.LineSeries.new(root, {
-          minBulletDistance: 10,
-          connect: false,
-          xAxis,
-          yAxis,
-          valueYField: yAxisName,
-          valueXField: this.xAxisName,
-          tooltip: am5.Tooltip.new(root, {}),
-        }),
-      );
-
-      series.get('tooltip').label.set('text', '{valueYField}: {valueY}');
-
-      series.strokes.template.setAll({
-        strokeWidth: 3,
-        templateField: 'strokeSettings',
-      });
-
-      // Make stuff animate on load
-/*
-      series.appear(1000, 100);
-      chart.appear(1000, 100);
-*/
-
-
-      // Process data
-      const processor = am5.DataProcessor.new(root, {
-        dateFields: [this.xAxisName],
-        dateFormat: this.xAxisFormat,
-        numericFields: [yAxisName],
-      });
-
-      processor.processMany(data);
-
-      // Assign parsed/processed data to series
-      series.data.setAll(data);
-
-      return () => root.dispose();
+      createChart(`neadAutoChart_${chartNumber}`, this.xAxisName, this.neadJSON?.parameters[chartNumber], series, this.xAxisFormat);
     },
   },
   data: () => ({
@@ -347,5 +247,74 @@ export const NEADAutoChart = () => ({
     loading: false,
     xAxisName: 'timestamp_iso',
     xAxisFormat: "yyyy-MM-dd H:m:s'+00:00'",
+  }),
+});
+
+export const EditingParameterViews = () => ({
+  components: { EditChart },
+  template: `
+    <v-col >
+    
+      <v-row class="py-3" >
+        EditChart
+      </v-row>
+
+      <v-row style="border: solid 1px;"
+             no-gutters>
+        <v-col >
+          <EditChart  />
+        </v-col>
+      </v-row>
+    
+    </v-col >
+  `,
+  mounted() {
+    // this.loadDataInJson(this.localNeadFile);
+    this.parseCSV(this.localNeadFile);
+  },
+  computed: {
+  },
+  methods: {
+    async loadDataInJson(url) {
+      this.loading = true;
+      // const neadContent = await axios.get(url);
+      const neadContent = await import(url);
+      this.loading = false;
+
+      const jsonData = convertCSVToJSON(neadContent.data, '-999.0');
+
+      return jsonData;
+    },
+    parseCSV(url) {
+      am5.net.load(url).then((result) => {
+
+        // Get responseType 'type' from response header, this indicates if external data is in JSON or CSV format
+        const responseType = this.getResponseType(result.type);
+
+        // Parse JSON data
+        if (responseType === 'application/json') {
+          this.parsedData = am5.JSONParser.parse(result.response);
+        } else if (responseType === 'text/csv') {
+
+          // TODO dynamically assign nullValue in call below, will need to parse nodata value from NEAD
+          const jsonConvert = convertCSVToJSON(result.response, '');
+          this.parsedData = jsonConvert.data;
+        } else {
+          console.log(`Error loading ${url}, response type ${responseType} is not compatible with application.`);
+        }
+
+      }).catch((error) => {
+        console.log(`Error loading data: ${error} for ${url}`);
+        console.log(error);
+      });
+    },
+  },
+  data: () => ({
+    parsedData: [],
+    loading: false,
+    // localNeadFile: `${__dirname}/../public/testdata/00-Swiss Camp 10m.csv`,
+    // localNeadFile: './public/researchUnits.json',
+    localNeadFile: 'public/testdata/00-Swiss Camp 10m.csv',
+    apiUrl1: 'https://www.envidat.ch/data-api/gcnet/json/swisscamp/airtemp2/2000-11-04T17:00:00/2020-11-10T00:00:00/',
   }),
 });
