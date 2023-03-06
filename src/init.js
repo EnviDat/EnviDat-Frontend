5315fe72080fe262076702535f1cd3d1e96e474c
Haas <dominik.haas@wsl.ch>
2023-03-06 12:46:23 +0100

    refactor(Telemetry): remove OTL dependencies

--- src/init.js
+++ src/init.js
@@ -11,15 +11,8 @@
  * file 'LICENSE.txt', which is part of this source code package. */
 
  import axios from 'axios';
- import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
- import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
- import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
- import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
- import { registerInstrumentations } from '@opentelemetry/instrumentation';
- import { ZoneContextManager } from '@opentelemetry/context-zone';
- import { Resource } from '@opentelemetry/resources';
- import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
- import { handleGenericAPIError, handleGenericError } from './factories/notificationFactory';
+
+import { handleGenericAPIError, handleGenericError } from './factories/notificationFactory';
 
  export const initAxios = (app, store) => {
    const storeRef = store;
@@ -84,35 +77,4 @@
        return Promise.reject(error);
      },
    );
- }
-
- // OTEL Instrumentation
- export const initOtel = (otelUrl) => {
-   const exporter = new OTLPTraceExporter({
-     url: otelUrl,
-   });
-   const provider = new WebTracerProvider({
-     resource: new Resource({
-       [SemanticResourceAttributes.SERVICE_NAME]: process.env.VITE_OTEL_NAME,
-     }),
-   });
-   provider.addSpanProcessor(new BatchSpanProcessor(exporter));
-   provider.register({
-     contextManager: new ZoneContextManager(),
-   });
-
-   registerInstrumentations({
-     instrumentations: [
-       getWebAutoInstrumentations({
-         // load custom configuration for xml-http-request instrumentation
-         '@opentelemetry/instrumentation-xml-http-request': {
-           propagateTraceHeaderCorsUrls: [/.+/g],
-         },
-         // load custom configuration for fetch instrumentation
-         '@opentelemetry/instrumentation-fetch': {
-           propagateTraceHeaderCorsUrls: [/.+/g],
-         },
-       }),
-     ],
-   });
  }
