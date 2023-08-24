124e18b09337c0a48b05eb9e5b0a6c37937b3b4a
Haas <dominik.haas@wsl.ch>
2023-08-24 08:35:26 +0200

    Added StepperInteractionView

---
+++ src/components/Navigation/StepperInteractionView.vue
@@ -0,0 +1,248 @@
+/* eslint-disable vue/no-unused-vars */
+<template>
+  <v-container fluid class="pa-0" >
+
+    <v-row >
+
+      <v-col v-if="showPreviewButton"
+             class="shrink">
+        <BaseIconButton
+                id="PreviewMetadataButton"
+                material-icon-name="remove_red_eye"
+                icon-color="black"
+                color="black"
+                outlined
+                tooltipText="Preview Dataset"
+                :tooltipBottom="true"
+                @clicked="catchPreviewClick"
+        />
+      </v-col>
+
+      <v-col v-if="isCreationWorkflow"
+              class="shrink">
+        <BaseIconButton
+                id="SaveMetadataButton"
+                material-icon-name="save"
+                icon-color="black"
+                color="black"
+                outlined
+                tooltipText="Save Dataset On the Server"
+                :disabled="!showSaveButton"
+                isFancy
+                :is-glowing="showSaveButton"
+                :tooltipBottom="true"
+                @clicked="catchSaveDatasetClick"
+        />
+      </v-col>
+
+      <v-col v-if="showProgress && isCreationWorkflow" >
+        <BaseProgressView :text="creationProgressInfo"
+                          :progress-pct="completedPct"
+                          color="secondary"
+        />
+      </v-col>
+
+      <v-col v-if="showProgress && !isCreationWorkflow" >
+          <BaseProgressView :text="editingProgressInfo"
+                            :progress-pct="completedPct"
+                            color="secondary"
+          />
+      </v-col>
+
+      <v-col class="shrink">
+
+        <BaseIconButton
+                id="MetadataEditCloseButton"
+                material-icon-name="close"
+                icon-color="black"
+                color="black"
+                outlined
+                tooltipText="Close workflow"
+                :tooltipBottom="true"
+                @clicked="catchCloseClick"
+        />
+      </v-col>
+
+    </v-row>
+
+    <v-row >
+      <v-container fluid class="fill-height py-0" >
+        <v-row no-gutters align-content="end">
+          <v-col v-if="message" >
+            <BaseStatusLabelView statusIcon="check"
+                                 statusColor="success"
+                                 :statusText="message"
+                                 :expandedText="messageDetails" />
+          </v-col>
+
+          <v-col v-if="error" >
+            <BaseStatusLabelView statusIcon="error"
+                                 statusColor="error"
+                                 :statusText="error"
+                                 :expandedText="errorDetails" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+/**
+
+ * @summary Stepper Interaction View
+ * @author Dominik Haas-Artho
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+*/
+
+import BaseIconButton from '@/components/BaseElements/BaseIconButton.vue';
+import BaseProgressView from '@/components/BaseElements/BaseProgressView.vue'
+
+import { countSteps } from '@/factories/userCreationFactory';
+
+export default {
+  name: 'StepperInteractionView',
+  props: {
+    steps: Array,
+    showPreviewButton: {
+      type: Boolean,
+      default: false,
+    },
+    showProgress: {
+      type: Boolean,
+      default: false,
+    },
+    showSaveButton: {
+      type: Boolean,
+      default: false,
+    },
+    isCreationWorkflow: {
+      type: Boolean,
+      default: false,
+    },
+    message: {
+      type: String,
+      default: '',
+    },
+    messageDetails: {
+      type: String,
+      default: null,
+    },
+    error: {
+      type: String,
+      default: '',
+    },
+    errorDetails: {
+      type: String,
+      default: null,
+    },
+  },
+  computed: {
+    completedPct() {
+      const pct = this.completedStepsAmount / this.allStepsAmount;
+      const pct2 = (pct * 100).toFixed(2);
+      return Number.parseFloat(pct2);
+    },
+    completedPctInteger() {
+      return Math.floor(this.completedPct);
+    },
+    completedStepsAmount() {
+      return countSteps(this.steps, true);
+    },
+    creationProgressInfo() {
+      if (this.completedPct >= 100) {
+        return 'Ready to save!'
+      }
+
+      if (this.completedPct >= 50) {
+        return 'A few infos more!'
+      }
+
+      return 'Enter all info to save!'
+    },
+    editingProgressInfo() {
+      if (this.completedPct >= 100) {
+        return 'Excellent!'
+      }
+
+      if (this.completedPct >= 70) {
+        return 'Almost there!'
+      }
+
+      return 'Provide more info!'
+    },
+    allStepsAmount() {
+      return countSteps(this.steps);
+    },
+  },
+  methods: {
+    catchCloseClick() {
+      this.$emit('clickedClose');
+    },
+    catchPreviewClick() {
+      this.$emit('clickedPreview');
+    },
+    catchSaveDatasetClick() {
+      this.$emit('clickedSaveDataset');
+    },
+  },
+  components: {
+    BaseIconButton,
+    BaseProgressView,
+  },
+};
+</script>
+
+<style scoped>
+.stepperContentGrid {
+  display: grid;
+  grid-template-columns: minmax(0, 1fr);
+  grid-template-rows: auto 72px minmax(auto, 1fr);
+  gap: 0;
+  grid-template-areas:
+    'infoPanel'
+    'stepper'
+    'content';
+  width: 100%;
+  height: 100%;
+}
+
+.infoPanel {
+  background-color: white;
+  border-radius: 5px;
+}
+
+.infoPanelGrid {
+  display: grid;
+  grid-template-rows: 1fr auto;
+  grid-template-columns: auto;
+  gap: 0;
+  grid-template-areas:
+  'instructions'
+  'interaction';
+}
+
+.infoPanelGrid-md {
+  display: grid;
+  grid-template-columns: 5fr auto;
+  gap: 30px;
+  grid-template-areas:
+  'instructions interaction';
+}
+
+.instructions {
+    display: flex;
+    justify-content: start;
+}
+
+.interaction {
+  display: flex;
+  justify-content: end;
+}
+
+.compactTitle {
+  line-height: 1.5rem !important;
+}
+</style>
