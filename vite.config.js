f3f43cda67486cd03f8f6b1d4a4ec7dd337df6e6
Dominik Haas <shaodiese@gmail.com>
2023-09-05 16:25:07 +0200

    added requireContext plugin to vite

--- vite.config.js
+++ vite.config.js
@@ -54,6 +54,7 @@ export default ({ mode }) => {
             vuetify({
               autoImport: true,
             }),
+            ViteRequireContext(),
             Unfonts({
               google: {
                 families: [
