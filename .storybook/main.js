96e11b972b5dcab3ea881054c22a79572ff269ba
Dominik Haas <dominik.haas@wsl.ch>
2023-09-07 16:01:08 +0200

    added mdx story for testing storyblocks

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@
 module.exports = {
   stories: [
   // '../stories/**/*.stories.mdx',
-  '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  '../stories/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: ['@storybook/addon-essentials'
   // '@storybook/addon-storysource',
   ],
