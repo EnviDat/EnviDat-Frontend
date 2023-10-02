601fdf33c019da040f06bae25d3d0186fb70f105
Dominik Haas <dominik.haas@wsl.ch>
2023-10-02 16:37:58 +0200

    refactor(vite-fonts-plugin): swapped font plugin to bundle it into the build

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { defineConfig, loadEnv } from 'vite';
 import { configDefaults } from 'vitest/dist/config.cjs';
 import eslint from 'vite-plugin-eslint';
 import ViteRequireContext from '@originjs/vite-plugin-require-context';
-import Unfonts from 'unplugin-fonts/vite'
+import ViteWebfontDownload from 'vite-plugin-webfont-dl'
 import { visualizer } from 'rollup-plugin-visualizer';
 
 import { getFilesWithPrefix } from './src/factories/enhancementsFactoryNode';
@@ -17,65 +17,64 @@ const version = process.env.npm_package_version;
 
 
 export default ({ mode, config }) => {
-    const isProd = mode === 'production'
+    const isProd = mode !== 'production'
 
-    if (isProd) {
-        const fileName = `version_${version}.txt`;
-        const existingFilePaths = path.resolve(__dirname, 'public/');
+    const fileName = `version_${version}.txt`;
+    const existingFilePaths = path.resolve(__dirname, 'public/');
 
-        const existingVersionFiles = getFilesWithPrefix(existingFilePaths, 'version_');
+    const existingVersionFiles = getFilesWithPrefix(existingFilePaths, 'version_');
 
-        // delete any existing files with version_ as prefix to make sure only the latest version is created
-        for (let i = 0; i < existingVersionFiles.length; i++) {
-            const file = existingVersionFiles[i];
-            const fullPath = path.resolve(`${existingFilePaths}`, `${file}`);
-            console.log(`Going to delete version file: ${fullPath}`);
-            fs.unlinkSync(fullPath);
-        }
+    // delete any existing files with version_ as prefix to make sure only the latest version is created
+    for (let i = 0; i < existingVersionFiles.length; i++) {
+        const file = existingVersionFiles[i];
+        const fullPath = path.resolve(`${existingFilePaths}`, `${file}`);
+        console.log(`Going to delete version file: ${fullPath}`);
+        fs.unlinkSync(fullPath);
+    }
 
-        const filePath = path.resolve(__dirname, 'public/', `${fileName}`);
+    const filePath = path.resolve(__dirname, 'public/', `${fileName}`);
 
-        try {
-            fs.writeFileSync(filePath, version);
-            console.log(`Created version file ${fileName} for easy build version highlight.`);
-        } catch (err) {
-            console.log(`Tried to created file ${fileName}. Error: ${err}`);
-        }
+    try {
+        fs.writeFileSync(filePath, version);
+        console.log(`Created version file ${fileName} for easy build version highlight in ${filePath}`);
+    } catch (err) {
+        console.log(`Tried to created file ${fileName} in ${filePath}. Error: ${err}`);
     }
 
     console.log(`starting server | version: ${version} | prod: ${isProd}`);
 
     return defineConfig({
         plugins: [
-            vue(),
-            eslint({
-              exclude: ['/virtual:/**', 'node_modules/**'],
-            }),
-            ViteRequireContext(),
-            Components({
-                resolvers: [
-                  // Vuetify
-                  VuetifyResolver(),
-                ],
-            }),
-            Unfonts({
-              google: {
-                families: [
-                  'Baskervville',
-                  {
-                    name: 'Raleway',
-                    styles: 'wght@400;500;700',
-                  },
-                ],
-              },
-            }),
+          vue(),
+          eslint({
+            exclude: ['/virtual:/**', 'node_modules/**'],
+          }),
+          ViteRequireContext(),
+          Components({
+              resolvers: [
+                // Vuetify
+                VuetifyResolver(),
+              ],
+          }),
+          ViteWebfontDownload(
+            ['https://fonts.googleapis.com/css2?family=Baskervville&family=Raleway:wght@400;500;700&display=swap'],
+            {
+              injectAsStyleTag: true,
+              minifyCss: true,
+              async: true,
+              cache: true,
+              proxy: false,
+            },
+          ),
           visualizer({
             filename: './dist/buildStats.html',
             title : 'EnviDat Build Visualizer',
           }),
         ],
         test: {
-          exclude: [...configDefaults.exclude, './tests/unit/ckanRegression.spec.js'],
+          exclude: [...configDefaults.exclude,
+            // './tests/unit/ckanRegression.spec.js',
+          ],
         },
         define: {
             'process.env': loadEnv(mode, process.cwd()),
