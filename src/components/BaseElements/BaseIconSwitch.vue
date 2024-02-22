e21c52534f7b2be7945b1c5f747d88f481089324
Benjamin Poole <benjamin.poole@wsl.ch>
2024-02-22 19:29:37 +0100

    Adjusted Resource Editing, clearer UX, cleaned up code as well

--- src/components/BaseElements/BaseIconSwitch.vue
+++ src/components/BaseElements/BaseIconSwitch.vue
@@ -1,30 +1,32 @@
 <template>
   <div class="baseIconSwitch">
-    <v-tooltip bottom >
-
+    <v-tooltip :disabled="!tooltipText" bottom>
       <template v-slot:activator="{ on }">
-        <button
-          tabindex="0"
-          type="button"
-          :disabled="disabled"
-          class="iconSwitchButton"
-          :id="'iconSwitchButton' + _uid"
-          :class="classList" 
-          :style="styleList"
-          role="switch"
-          :aria-labelledby="'iconSwitchLabel' + _uid"
-          :aria-checked="active"
-          @click="emitClick"
-          v-on="on"
-        >
-          <v-icon v-if="materialIconName" class="iconSwitchButtonIcon" :color="iconColor">
-            {{ materialIconName }}
-          </v-icon>
-        </button>
+        <div class="d-flex" v-on="on">
+          <div class="iconSwitch">
+            <button
+              tabindex="0"
+              type="button"
+              :disabled="disabled"
+              class="iconSwitchButton"
+              :id="'iconSwitchButton' + _uid"
+              :class="classList"
+              :style="styleList"
+              role="switch"
+              :aria-describedby="'iconSwitchLabel' + _uid"
+              :aria-checked="active"
+              @click="emitClick"
+              
+            >
+              <v-icon v-if="materialIconName" class="iconSwitchButtonIcon" :color="iconColor">
+                {{ materialIconName }}
+              </v-icon>
+            </button>
+          </div>
+          <label v-if="label" :for="'iconSwitchButton' + _uid" class="iconSwitchLabel ml-2" :class="{disabled}">{{ label }}</label>
+        </div>
       </template>
-
-      <label :for="'iconSwitchButton' + _uid">{{ tooltipText }}</label>
-
+      <span v-if="tooltipText">{{ tooltipText }}</span>
     </v-tooltip>
   </div>
 </template>
@@ -51,6 +53,10 @@ export default {
       type: Boolean,
       default: false,
     },
+    label: {
+      type: String,
+      default: undefined,
+    },
     color: {
       type: String,
       default: 'primary',
@@ -74,17 +80,12 @@ export default {
     internalActive: false,
   }),
   computed: {
-    styleList() {
-      return {
-
-      }
-    },
     classList(){
       return {
-        'clickable': !this.disabled,
-        'active': this.active,
+        disabled: this.disabled,
+        active: this.active,
         [`${this.color}--text text--lighten-2`]: this.internalActive,
-        'grey--text text--lighten-1': !this.internalActive,
+        'grey--text text--lighten-2': !this.internalActive,
       }
     },
     iconColor(){
@@ -113,52 +114,68 @@ $button-shadow: 0 2px 4px -1px rgba(0,0,0,.2),0 4px 5px 0 rgba(0,0,0,.14),0 1px 
 $slide-duration: 0.2s;
 
 .baseIconSwitch {
-  position: relative;
-  z-index: 0;
-  width: $switch-length;
-  height: $button-size;
-  .iconSwitchButton {
-    margin: 0;
-    padding: 0;
-    box-shadow: $button-shadow;
-    border-radius: 50%;
-    background-color: #fff;
-    height: $button-size;
-    width: $button-size;
-    left: 0;
-    transition: left $slide-duration ease-in-out;
-
-    &.active {
-      $activeDelta: $switch-length - $button-size;
-      position: absolute;
-      left: $activeDelta;
-      transition: left $slide-duration ease-in-out;
-      &:before {
-        margin-left: -$activeDelta;
-        transition: margin-left $slide-duration ease-in-out, background-color $slide-duration;
-      }
+  display: flex;
+  flex-wrap: nowrap;
+  .iconSwitchLabel {
+    user-select: none;
+    &.disabled {
+      color: rgba(0, 0, 0, 0.38);
     }
+  }
 
-    &.clickable {
+  .iconSwitch {
+    position: relative;
+    z-index: 0;
+    min-width: $switch-length;
+    min-height: $button-size + 4px;
+    .iconSwitchButton {
+      position: absolute;
+      border-radius: 50%;
+      height: $button-size;
+      width: $button-size;
+      left: 0;
+      box-shadow: $button-shadow;
+      background-color: #FFF;
+      transition: none;
+
       &:hover {
         box-shadow: $button-shadow, 0 0 0 4px rgba(33,33,33,0.2);
       }
-    }
 
-    // BG
-    &:before {
-      content: ' ';
-      position: absolute;
-      display: block; 
-      width: $switch-length;
-      height: $switch-bg-height;
-      border-radius: 8px; 
-      top: ($button-size - $switch-bg-height) / 2;
-      background-color: currentColor;
-      box-shadow: inset 1px 1px 3px rgba(33, 33, 33, 0.2);
-      z-index: -1; // Behind the button
-    }
+      &.active {
+        $activeDelta: $switch-length - $button-size;
+        left: $activeDelta;
+        transition: left $slide-duration ease-in-out;
+        &:before {
+          margin-left: -$activeDelta;
+          transition: margin-left $slide-duration ease-in-out, background-color $slide-duration;
+        }
+      }
 
+      &.disabled {
+        background-color: #DDD;
+        cursor: not-allowed;
+        box-shadow: 0 0 1px 1px rgba(33, 33, 33, 0.1);
+        &:before {
+          box-shadow: none;
+        }
+      }
+
+      // BG
+      &:before {
+        content: ' ';
+        position: absolute;
+        display: block; 
+        width: $switch-length;
+        height: $switch-bg-height;
+        border-radius: 8px; 
+        top: ($button-size - $switch-bg-height) / 2;
+        background-color: currentColor;
+        z-index: -1; // Behind the button
+        box-shadow: inset 1px 1px 3px rgba(33, 33, 33, 0.2);
+      }
+
+    }
   }
 }
 
