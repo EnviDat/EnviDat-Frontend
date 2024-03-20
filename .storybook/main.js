d2223bd3092951f8d40634044296a4214a00f2d2
Dominik Haas <dominik.haas@wsl.ch>
2024-03-20 09:24:17 +0100

    update to storybook 8.0.2

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,17 @@
 
 export default {
   stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+
   addons: [
     '@storybook/addon-storysource',
     '@storybook/addon-essentials',
   ],
+
   framework: {
     name: '@storybook/vue3-vite',
     options: {},
   },
-  core: {
-    builder: '@storybook/builder-vite',
-  },
+
   docs: {
     autodocs: true,
   }
