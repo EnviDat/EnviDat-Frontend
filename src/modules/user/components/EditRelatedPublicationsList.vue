e98e42ec528062be8c357f959bfd859dbbe9feef
enricoperuselli <enrico.peruselli@wsl.ch>
2024-07-23 10:05:02 +0200

    MetadaHeader - Added author and tag accordion

--- src/modules/user/components/EditRelatedPublicationsList.vue
+++ src/modules/user/components/EditRelatedPublicationsList.vue
@@ -1,88 +1,75 @@
 <template>
+  <v-card id="EditRelatedPublicationsList" class="pa-0" :loading="loading">
+    <v-container fluid class="pa-4 fill-height">
+      <template slot="progress">
+        <v-progress-linear color="primary" indeterminate />
+      </template>
 
-<v-card id="EditRelatedPublicationsList"
-        class="pa-0"
-        :loading="loading">
+      <v-row>
+        <v-col cols="6" class="text-h5">
+          {{ EDIT_METADATA_RELATEDPUBLICATIONS_TITLE }}
+        </v-col>
 
-  <v-container fluid
-                class="pa-4 fill-height" >
+        <v-col v-if="message">
+          <BaseStatusLabelView
+            statusIcon="check"
+            statusColor="success"
+            :statusText="message"
+            :expandedText="messageDetails"
+          />
+        </v-col>
+        <v-col v-if="error">
+          <BaseStatusLabelView
+            statusIcon="error"
+            statusColor="error"
+            :statusText="error"
+            :expandedText="errorDetails"
+          />
+        </v-col>
+      </v-row>
 
-    <template slot="progress">
-      <v-progress-linear color="primary"
-                         indeterminate />
-    </template>
+      <v-row>
+        <v-col>
+          <div class="text-subtitle-1" v-html="labels.cardInstructions"></div>
+        </v-col>
+      </v-row>
 
-    <v-row>
-      <v-col cols="6" class="text-h5">
-        {{ EDIT_METADATA_RELATEDPUBLICATIONS_TITLE }}
-      </v-col>
+      <v-row no-gutters class="pt-4">
+        <v-col>
+          <BaseStatusLabelView
+            status-icon="question_mark"
+            status-text="More Info"
+            expandedText="instructions for adding"
+            :show-expand-icon="true"
+          />
+        </v-col>
+      </v-row>
 
-      <v-col v-if="message" >
-        <BaseStatusLabelView statusIcon="check"
-                             statusColor="success"
-                             :statusText="message"
-                             :expandedText="messageDetails" />
-      </v-col>
-      <v-col v-if="error"  >
+      <v-row>
+        <v-col>
+          <EditAddPublication
+            :pid="previewPid"
+            :doi="previewDoi"
+            dense
+            @addClicked="catchAddPublication"
+          />
+        </v-col>
+      </v-row>
 
-        <BaseStatusLabelView statusIcon="error"
-                             statusColor="error"
-                             :statusText="error"
-                             :expandedText="errorDetails" />
-      </v-col>
+      <v-row no-gutters class="pt-4">
+        <v-col>
+          <div class="text-subtitle-1" v-html="labels.preview"></div>
+        </v-col>
+      </v-row>
 
-    </v-row>
-
-    <v-row>
-      <v-col >
-        <div class="text-subtitle-1"
-              v-html="labels.cardInstructions">
-
-        </div>
-      </v-col>
-    </v-row>
-
-    <v-row no-gutters
-           class="pt-4">
-      <v-col >
-        <BaseStatusLabelView status-icon="question_mark"
-                             status-text="More Info"
-                             expandedText="instructions for adding"
-                             :show-expand-icon="true"
-        />
-      </v-col>
-    </v-row>
-
-    <v-row>
-      <v-col >
-        <EditAddPublication :pid="previewPid"
-                            :doi="previewDoi"
-                            dense
-                            @addClicked="catchAddPublication" />
-      </v-col>
-    </v-row>
-
-    <v-row no-gutters
-           class="pt-4">
-      <v-col >
-        <div class="text-subtitle-1"
-             v-html="labels.preview">
-
-        </div>
-      </v-col>
-    </v-row>
-
-    <v-row>
-      <v-col >
-         <MetadataPublications v-bind="publicationsObject" />
-      </v-col>
-    </v-row>
-
- </v-container>
-</v-card>
-
+      <v-row>
+        <v-col>
+          <MetadataPublications v-bind="publicationsObject" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
-
 
 <script>
 /**
@@ -94,7 +81,7 @@
  *
  * This file is subject to the terms and conditions defined in
  * file 'LICENSE.txt', which is part of this source code package.
-*/
+ */
 import {
   EDITMETADATA_CLEAR_PREVIEW,
   EDITMETADATA_OBJECT_UPDATE,
@@ -105,11 +92,13 @@ import {
 import BaseStatusLabelView from '@/components/BaseElements/BaseStatusLabelView.vue';
 
 import { EDIT_METADATA_RELATEDPUBLICATIONS_TITLE } from '@/factories/metadataConsts';
-import { getValidationMetadataEditingObject, isFieldValid } from '@/factories/userEditingValidations';
+import {
+  getValidationMetadataEditingObject,
+  isFieldValid,
+} from '@/factories/userEditingValidations';
 
 import EditAddPublication from '@/modules/user/components/EditAddPublication.vue';
 import MetadataPublications from '@/modules/metadata/components/Metadata/MetadataPublications.vue';
-
 
 export default {
   name: 'EditRelatedPublicationsList',
@@ -169,7 +158,9 @@ export default {
       return this.previewText ? this.previewText : this.relatedPublicationsText;
     },
     validations() {
-      return getValidationMetadataEditingObject(EDITMETADATA_RELATED_PUBLICATIONS);
+      return getValidationMetadataEditingObject(
+        EDITMETADATA_RELATED_PUBLICATIONS,
+      );
     },
   },
   methods: {
@@ -195,11 +186,15 @@ export default {
         this.setRelatedPublicationsText(value);
       }
     },
-    validateProperty(property, value){
-      return isFieldValid(property, value, this.validations, this.validationErrors);
+    validateProperty(property, value) {
+      return isFieldValid(
+        property,
+        value,
+        this.validations,
+        this.validationErrors,
+      );
     },
     setRelatedPublicationsText(value) {
-
       eventBus.emit(EDITMETADATA_OBJECT_UPDATE, {
         object: EDITMETADATA_RELATED_PUBLICATIONS,
         data: { [this.editingProperty]: value },
@@ -213,9 +208,11 @@ export default {
     editingProperty: 'relatedPublicationsText',
     EDIT_METADATA_RELATEDPUBLICATIONS_TITLE,
     labels: {
-      cardInstructions: 'Add DORA links to other publications, you can find them on <a href="https://www.dora.lib4ri.ch/wsl/" target="_blank">dora lib4ri</a> or directly enter DORA permanent IDs ex. wsl:29664). Click into the text arena for examples.',
-      placeholder: 'Example entries: \n  * wsl:18753 \n' +
-          ' * https://www.dora.lib4ri.ch/wsl/islandora/object/wsl:18753 ',
+      cardInstructions:
+        'Add DORA links to other publications, you can find them on <a href="https://www.dora.lib4ri.ch/wsl/" target="_blank">dora lib4ri</a> or directly enter DORA permanent IDs ex. wsl:29664). Click into the text arena for examples.',
+      placeholder:
+        'Example entries: \n  * wsl:18753 \n' +
+        ' * https://www.dora.lib4ri.ch/wsl/islandora/object/wsl:18753 ',
       preview: 'Preview of the Related Publications',
     },
     publicationsMap: null,
@@ -232,6 +229,4 @@ export default {
     BaseStatusLabelView,
   },
 };
-
-
 </script>
---
+++ stories/metadataHeader.stories.js
@@ -0,0 +1,77 @@
+import MetadataHeader from '@/modules/metadata/components/Metadata/MetadataHeader.vue';
+import { createHeader } from '@/factories/metaDataFactory';
+import metadata from './js/metadata';
+
+const smallHeader = createHeader(metadata[0], true);
+const largeHeader = createHeader(metadata[2], false);
+const longAuthorsListHeader = createHeader(metadata[3], false);
+
+export default {
+  title: '15 Metadata Header / Header Views',
+  component: MetadataHeader,
+};
+
+// export const Empty = { args: {} };
+
+export const EmptyMetadataHeader = {
+  args: {},
+};
+
+export const PlaceHolderMetadataHeader = {
+  args: {
+    showPlaceholder: true,
+  },
+};
+
+export const ShortTitleMetadataHeader = {
+  args: {
+    ...smallHeader,
+  },
+  argTypes: {
+    checkSize: { action: 'checkSize' },
+    clickedTag: { action: 'clickedTag' },
+    clickedAuthor: { action: 'clickedAuthor' },
+    clickedBack: { action: 'clickedBack' },
+    clickedEdit: { action: 'clickedEdit' },
+  },
+};
+
+export const LongTitleMetadataHeader = {
+  args: {
+    ...largeHeader,
+    showEditButton: true,
+  },
+  argTypes: {
+    checkSize: { action: 'checkSize' },
+    clickedTag: { action: 'clickedTag' },
+    clickedAuthor: { action: 'clickedAuthor' },
+    clickedBack: { action: 'clickedBack' },
+    clickedEdit: { action: 'clickedEdit' },
+  },
+};
+
+export const LongTitleNoIconsMetadataHeader = {
+  args: {
+    ...largeHeader,
+  },
+  argTypes: {
+    checkSize: { action: 'checkSize' },
+    clickedTag: { action: 'clickedTag' },
+    clickedAuthor: { action: 'clickedAuthor' },
+    clickedBack: { action: 'clickedBack' },
+    clickedEdit: { action: 'clickedEdit' },
+  },
+};
+
+export const CropAuthorsListMetadataHeader = {
+  args: {
+    ...longAuthorsListHeader,
+  },
+  argTypes: {
+    checkSize: { action: 'checkSize' },
+    clickedTag: { action: 'clickedTag' },
+    clickedAuthor: { action: 'clickedAuthor' },
+    clickedBack: { action: 'clickedBack' },
+    clickedEdit: { action: 'clickedEdit' },
+  },
+};
