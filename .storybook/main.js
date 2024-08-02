b7f91cc2af5a1068414c8c6b911ec0d9c08694c0
enricoperuselli <enrico.peruselli@wsl.ch>
2024-08-02 12:49:53 +0200

    Fixed proxy storybook matomo

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,35 @@
+import { mergeConfig } from 'vite';
 
 module.exports = {
   stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: [
-    '@storybook/addon-storysource',
-    '@storybook/addon-essentials',
-  ],
+  addons: ['@storybook/addon-storysource', '@storybook/addon-essentials'],
   framework: {
     name: '@storybook/vue-vite',
     options: {},
   },
   docs: {
     autodocs: true,
-  }
+  },
+
+  // reference
+  // https://stackoverflow.com/questions/76297669/nx-16-cant-configure-a-proxy-in-storybook-vite-and-react-library
+  // https://storybook.js.org/docs/builders/vite
+
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      optimizeDeps: {
+        include: ['storybook-dark-mode'],
+      },
+      server: {
+        proxy: {
+          '/api': {
+            target: 'https://statistics.wsl.ch',
+            changeOrigin: true,
+            secure: false,
+            rewrite: path => path.replace(/^\/api/, ''),
+          },
+        },
+      },
+    });
+  },
 };
