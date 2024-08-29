5ac0f684a1716f7b2751d673d356de94bfd08cb8
Haas <dominik.haas@wsl.ch>
2024-08-29 17:33:28 +0200

    fixed resourceCard stories
    # Conflicts:
    #	stories/resourceCardCollections.stories.js

--- stories/resourceCardCollections.stories.js
+++ stories/resourceCardCollections.stories.js
@@ -4,6 +4,11 @@ import ResourceCardPlaceholder from '@/modules/metadata/components/ResourceCardP
 
 import unFormatedMetadataCards from './js/metadata';
 
+import dateCreatedIcon from '../src/assets/icons/dateCreated.png';
+import lastModifiedIcon from '../src/assets/icons/dateModified.png';
+import doiIcon from '../src/assets/icons/doi.png';
+import fileSizeIcon from '../src/assets/icons/fileSize.png';
+
 const metadataCards = unFormatedMetadataCards;
 
 
@@ -21,50 +26,63 @@ export const ResourceCardCollection = () => ({
   components: { ResourceCard },
   template: `
     <v-row>
-      <v-col
-        cols='3'
-        class='pa-2'
-        v-for='(res, index) in metadataCards[2].resources'
-        :key="'cols-3_' + index"
-      >
-        <resource-card v-bind='res' />
+      <v-col cols="3" class="pa-2" v-for="(res, index) in metadataCards[2].resources" :key="'1_' + index" >
+        <resource-card
+          v-bind="res"
+        />
       </v-col>
 
-      <v-col
-        cols='4'
-        class='pa-2'
-        v-for='(res, index) in metadataCards[2].resources'
-        :key="'cols-4_' + index"
-      >
-        <resource-card v-bind='res' is-protected />
+      <v-col cols="4" class="pa-2" v-for="(res, index) in metadataCards[2].resources" :key="'2_' + index" >
+        <resource-card
+          v-bind="res"
+          :doiIcon="doiIcon"
+          :dateCreatedIcon="dateCreatedIcon"
+          :lastModifiedIcon="lastModifiedIcon"
+          :isProtected="true"
+        />
       </v-col>
 
-      <v-col
-        cols='6'
-        class='pa-2'
-        v-for='(res, index) in metadataCards[2].resources'
-        :key="'cols-6_' + index"
-      >
-        <resource-card v-bind='res' />
+      <v-col cols="12" class="pa-2" v-for="(res, index) in metadataCards[2].resources" :key="'3_' + index" >
+        <resource-card
+          v-bind="res"
+          :doiIcon="doiIcon"
+          :dateCreatedIcon="dateCreatedIcon"
+          :lastModifiedIcon="lastModifiedIcon"
+        />
       </v-col>
 
-      <v-col cols='12' class='pa-2'>
-        Resource Cards with Download disabled
+      <v-col cols="3" class="pa-2" v-for="(res, index) in metadataCards[2].resources" :key="'4_' + index" >
+        <resource-card
+          v-bind="res"
+          :doiIcon="doiIcon"
+          :dateCreatedIcon="dateCreatedIcon"
+          :lastModifiedIcon="lastModifiedIcon"
+          :deprecated="true"
+        />
       </v-col>
 
-      <v-col
-        cols='6'
-        class='pa-2'
-        v-for='(res, index) in metadataCards[0].resources'
-        :key="'cols-6_' + index"
-      >
-        <resource-card v-bind='res' :downloadActive='false' />
+      <v-col cols="12" class="pa-2 my-2" >
+        <h2>Resource Cards with Download disabled</h2>
+      </v-col>
+
+      <v-col cols="6" class="pa-2" v-for="(res, index) in metadataCards[0].resources" :key="'5_' + index" >
+        <resource-card
+          v-bind="res"
+          :doiIcon="doiIcon"
+          :dateCreatedIcon="dateCreatedIcon"
+          :lastModifiedIcon="lastModifiedIcon"
+          :downloadActive="false"
+        />
       </v-col>
 
     </v-row>
   `,
-  data: () => ({
-    metadataCards,
+data: () => ({
+  metadataCards,
+  doiIcon,
+  fileSizeIcon,
+  dateCreatedIcon,
+  lastModifiedIcon,
   }),
 });
 
@@ -127,5 +145,35 @@ export const ResourceCardPlaceholders = () => ({
 
     </v-row>
     `,
-    data: () => ({}),
-  });
+  data: () => ({}),
+});
+
+export const ResourceCardDownloaded = () => ({
+  components: { ResourceCard },
+  template: `
+    <v-row >
+
+      <v-col cols="3" class="pa-2"
+              v-for="(res, index) in resources"
+              :key="'cols-3_' + index" >
+
+              <resource-card v-bind="res"
+                              :doiIcon="doiIcon"
+                              :dateCreatedIcon="dateCreatedIcon"
+                              :lastModifiedIcon="lastModifiedIcon"
+                              :showGenericOpenButton="true"
+                              :genericOpenButtonBottom="true"
+                              openButtonTooltip="Open File in Preview"
+                               />
+      </v-col>
+
+    </v-row>
+    `,
+  data: () => ({
+    resources: resources1.resources,
+    doiIcon,
+    dateCreatedIcon,
+    lastModifiedIcon,
+  }),
+});
+
