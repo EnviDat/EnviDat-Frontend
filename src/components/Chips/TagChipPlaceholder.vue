9c4d87484b72bccebb403fe03d33de3a56b52a0a
Dominik Haas <dominik.haas@wsl.ch>
2024-09-30 14:24:58 +0200

    fix: no white showing for placeholder tags

--- src/components/Chips/TagChipPlaceholder.vue
+++ src/components/Chips/TagChipPlaceholder.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-skeleton-loader type="chip" height="20" width="80" class="ma-1" />
+  <v-skeleton-loader type="chip" height="20" width="80" class="ma-1" color="transparent" />
 </template>
 <script>
 /**
