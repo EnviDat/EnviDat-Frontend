c47de3014e37dbce57cd573cf6a0d1e62ce3525f
Dominik Haas <dominik.haas@wsl.ch>
2024-10-16 18:44:33 +0200

    changed gradient for the author card to be more subtle

--- src/modules/metadata/components/AuthorCard.vue
+++ src/modules/metadata/components/AuthorCard.vue
@@ -465,7 +465,7 @@ export default {
       const color = this.colorPalette[from];
       const toColor = this.colorPaletteTo[to];
 
-      return `background-image: linear-gradient(45deg, ${color} 10%, ${toColor} 90%);
+      return `background-image: linear-gradient(310deg, ${color} 10%, ${toColor} 45%);
               background-position: center, center; background-size: cover;`;
     },
     authorIsDead() {
