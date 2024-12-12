0d24ef0943d2ee75b7e2507980189c69796113be
Haas <dominik.haas@wsl.ch>
2024-12-12 11:14:47 +0100

    fixed unit test for organizationTree

--- tests/unit/organizationFactory.spec.js
+++ tests/unit/organizationFactory.spec.js
@@ -1,20 +1,23 @@
 import { it, describe, expect } from 'vitest';
 
-import { getOrganizationTree } from '@/factories/organizationFactory';
+import { getOrganizationMap, getOrganizationTree } from '@/factories/organizationFactory';
 
 import orgaMapProd from '../../stories/testdata/orgaMapProd.json';
 
 describe('organizationFactory - getOrganizationTree', () => {
   it('recursive OrganizationTree', () => {
 
-    const organiztionMap = new Map();
     const orgaEntries = Object.values(orgaMapProd);
+    let orgas = [];
+    orgaEntries.forEach((entry) => {
+      orgas = [...orgas, ...entry.value];
+    });
 
-    orgaEntries.map((entry) => organiztionMap.set(entry.key, entry.value));
-
-    const tree = getOrganizationTree(organiztionMap);
+    const orgaMap = getOrganizationMap(orgas);
+    const tree = getOrganizationTree(orgaMap);
 
     expect(tree).toBeDefined();
+    expect(tree.length > 0).toBeTruthy();
   });
 
 });
