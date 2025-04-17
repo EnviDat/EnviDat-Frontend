60707749768d80d28581f5983072d619c9bd49b1
Dominik Haas <dominik.haas@wsl.ch>
2025-04-17 16:22:10 +0200

    added basic plot example

---
+++ src/components/Charts/PlotChart.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import * as Plot from '@observablehq/plot';
+import PlotFigure from './PlotFigure';
+import penguins from './penguins.json';
+</script>
+
+<template>
+  <div>
+    <h1>Plot</h1>
+
+    <PlotFigure
+      :options="{
+        marks: [
+          Plot.dot(penguins, {x: 'culmen_length_mm', y: 'culmen_depth_mm'}),
+        ],
+      }"
+    />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ stories/plotChart.stories.js
@@ -0,0 +1,11 @@
+import PlotChart from '@/components/Charts/PlotChart.vue';
+
+export default {
+  title: '1 Base / Charts / Plot Chart',
+  component: PlotChart,
+};
+
+export const Test = {
+  args: {
+  },
+}
