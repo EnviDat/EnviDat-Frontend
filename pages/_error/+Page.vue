b421ca8dc2b2703a448d0bdc5a227550fdc09900
Dominik Haas <dominik.haas@wsl.ch>
2025-06-23 12:38:31 +0200

    WIP: added errorPage for local redirect; added VITE_SEO_BASE to load JS dependcies correctly with the vike build;

---
+++ pages/_error/+Page.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { usePageContext } from 'vike-vue/usePageContext';
+
+const pageContext = usePageContext();
+
+const queryString = pageContext.urlOriginal?.split('?')[1];
+
+const datasetUrl = queryString || '';
+
+</script>
+
+<template>
+  <div>
+
+    <h1>Site Not Found</h1>
+
+    <div>Did you want to go to </div>
+    <div>{{ datasetUrl }}</div>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
