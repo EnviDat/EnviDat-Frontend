1262782fa7a45fd0fcac65fe3845bb17bac83234
Haas <dominik.haas@wsl.ch>
2025-10-22 12:35:27 +0200

    chore: vite update; storybook stories reset to load all for the building

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,9 +3,9 @@ import { mergeConfig } from 'vite';
 const allStories = ['../stories/**/*.stories.@(js|jsx|ts|tsx)'];
 
 const storiesToLoad = [
-  // '../stories/**/*.stories.@(js|jsx|ts|tsx)', // all stories
-  '../stories/baseElements/**/*.stories.@(js|jsx|ts|tsx)', // base components
-  '../stories/workflow/**/*.stories.@(js|jsx|ts|tsx)', // workflow components
+  '../stories/**/*.stories.@(js|jsx|ts|tsx)', // all stories
+  // '../stories/baseElements/**/*.stories.@(js|jsx|ts|tsx)', // base components
+  // '../stories/workflow/**/*.stories.@(js|jsx|ts|tsx)', // workflow components
   // '../stories/dataset/**/*.stories.@(js|jsx|ts|tsx)', // create & edit dataset
   // '../stories/search/**/*.stories.@(js|jsx|ts|tsx)', // search components
   // '../stories/navigation/**/*.stories.@(js|jsx|ts|tsx)', // navigation components
